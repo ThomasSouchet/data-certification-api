c45d397b8039988cab0110b12e18ed8c14d398c5
Thomas Souchet <thomas.souchet@gmail.com>
2021-06-30 17:43:59 +0200

    API: start to implement predict endpoint

--- api/app.py
+++ api/app.py
@@ -11,3 +11,25 @@ def index():
 
 
 # Implement a /predict endpoint
+@app.get("/predict")
+def predict(acousticness: str, danceability: str, duration_ms: str, energy: str, explicit: str, id: str, instrumentalness: str, key: str, liveness: str, loudness: str, mode: str, name: str, release_date: str, speechiness: str, tempo: str, valence: str, artist: str):
+
+    return {
+        "acousticness": acousticness, 
+        "danceability": danceability, 
+        "duration_ms": duration_ms, 
+        "energy": energy, 
+        "explicit": explicit, 
+        "id": id, 
+        "instrumentalness": instrumentalness, 
+        "key": key, 
+        "liveness": liveness, 
+        "loudness": loudness, 
+        "mode": mode, 
+        "name": name, 
+        "release_date": release_date, 
+        "speechiness": speechiness, 
+        "tempo": tempo, 
+        "valence": valence, 
+        "artist": artist
+    }
